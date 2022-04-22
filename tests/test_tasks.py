ec867d0881b9947365ce09713a27887beb94159e
Alessandro Lollo <alessandro.lollo@cloudacademy.com>
2022-04-22 09:23:05 +0200

    Added more tests

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,7 +1,10 @@
 import pytest
 from prefect import flow
+from typing import Optional
+from transform.models import MqlMaterializeResp, MqlQueryStatusResp, MqlQueryStatus
+from unittest import mock
 
-from prefect_transform.exceptions import TransformConfigurationException
+from prefect_transform.exceptions import TransformConfigurationException, TransformAuthException, TransformRuntimeException
 from prefect_transform.tasks import create_materialization
 
 
@@ -54,4 +57,55 @@ def test_missing_materialization_name_raises():
 
     msg_match = "`materialization_name` is missing."
     with pytest.raises(TransformConfigurationException, match=msg_match):
+        test_flow().result().result()
+
+def test_raises_on_connection_exception():
+    @flow
+    def test_flow():
+        return create_materialization(api_key="key", mql_server_url="url", materialization_name="mt_name")
+    
+    msg_match = "Cannot connect to Transform server!"
+    with pytest.raises(TransformAuthException, match=msg_match):
+        test_flow().result().result()
+
+@mock.patch("prefect_transform.tasks.MQLClient")
+def test_run_raises_on_create_materialization_async(mock_mql_client):
+    error_msg = "Error while creating async materialization!"
+
+    class MockMQLClient:
+        def create_materialization(
+            materialization_name: str,
+            start_time: Optional[str] = None,
+            end_time: Optional[str] = None,
+            model_key_id: Optional[int] = None,
+            output_table: Optional[str] = None,
+            force: bool = False,
+        ):
+            return MqlQueryStatusResp(
+                query_id="xyz",
+                status=MqlQueryStatus.FAILED,
+                sql="sql_query",
+                error=error_msg,
+                chart_value_max=None,
+                chart_value_min=None,
+                result=None,
+                result_primary_time_granularity=None,
+                result_source=None,
+            )
+
+    mock_mql_client.return_value = MockMQLClient
+
+    @flow
+    def test_flow():
+        return create_materialization(
+            api_key="key",
+            mql_server_url="url",
+            materialization_name="mt_name",
+            wait_for_creation=False
+        )
+
+    msg_match = (
+        f"Transform materialization async creation failed! Error is: {error_msg}"
+    )
+    with pytest.raises(TransformRuntimeException, match=msg_match):
         test_flow().result().result()
